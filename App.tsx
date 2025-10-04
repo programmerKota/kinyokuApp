374418bbce20e131d2b226d6accfdb993a1ef5f5
Kota (local) <programmerkota07@gmail.com>
2025-10-04 19:08:46 +0900

    ネットワーク接続☑不要のため削除

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { AuthProvider } from "@app/contexts/AuthContext";
 import RootNavigator from "@app/navigation/RootNavigator";
 import ErrorBoundary from "@shared/components/ErrorBoundary";
-import ConnectivityBadge from "@shared/components/ConnectivityBadge";
 import { initSupabaseAuthDeepLinks } from "@core/services/supabase/authService";
 import { AuthPromptProvider } from "@shared/auth/AuthPromptProvider";
 
@@ -21,7 +20,6 @@ const App = () => {
           </AuthProvider>
         </AuthPromptProvider>
       </ErrorBoundary>
-      {__DEV__ && <ConnectivityBadge />}
     </GestureHandlerRootView>
   );
 };
